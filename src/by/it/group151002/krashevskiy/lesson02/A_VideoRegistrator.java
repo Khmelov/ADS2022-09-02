924b4f7a8fd0f35d16a0a80f153b1fcd49a1822a
Nikita Krashevskiy <nikita.krashevskiy@gmail.com>
2022-09-11 00:32:26 +0300

    lesson 01 and lesson 02

---
+++ src/by/it/group151002/krashevskiy/lesson02/A_VideoRegistrator.java
@@ -0,0 +1,47 @@
+package by.it.group151002.krashevskiy.lesson02;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+/*
+даны события events
+реализуйте метод calcStartTimes, так, чтобы число включений регистратора на
+заданный период времени (1) было минимальным, а все события events
+были зарегистрированы.
+Алгоритм жадный. Для реализации обдумайте надежный шаг.
+*/
+
+public class A_VideoRegistrator {
+
+    public static void main(String[] args) {
+        A_VideoRegistrator instance=new A_VideoRegistrator();
+        double[] events=new double[]{1, 1.1, 1.6, 2.2, 2.4, 2.7, 3.9, 8.1, 9.1, 5.5, 3.7};
+        List<Double> starts=instance.calcStartTimes(events,1); //рассчитаем моменты старта, с длинной сеанса 1
+        System.out.println(starts);                            //покажем моменты старта
+    }
+    //модификаторы доступа опущены для возможности тестирования
+    List<Double> calcStartTimes(double[] events, double workDuration){
+        //events - события которые нужно зарегистрировать
+        //timeWorkDuration время работы видеокамеры после старта
+        List<Double> result = new ArrayList<>();
+        Arrays.sort(events);
+        double firstEvent = events[0];
+        result.add(firstEvent);
+        firstEvent += 1;
+        for (int i = 1; i < events.length; i++) {
+            if (firstEvent < events[i]) {
+                firstEvent = events[i];
+                result.add(firstEvent);
+                firstEvent += 1;
+            }
+        }
+
+        //комментарии от проверочного решения сохранены для подсказки, но вы можете их удалить.
+        //подготовка к жадному поглощению массива событий
+        //hint: сортировка Arrays.sort обеспечит скорость алгоритма
+        //C*(n log n) + C1*n = O(n log n)
+
+        return result;                        //вернем итог
+    }
+
+}
