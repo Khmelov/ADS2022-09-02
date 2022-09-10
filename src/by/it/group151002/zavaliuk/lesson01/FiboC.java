a4baee75770b0b8d4e2a13da4cd7182702e9c075
Daria <dar.zavaliuk@gmail.com>
2022-09-10 15:02:11 +0300

    feat: add solution to task C

--- src/by/it/group151002/zavaliuk/lesson01/FiboC.java
+++ src/by/it/group151002/zavaliuk/lesson01/FiboC.java
@@ -16,16 +16,41 @@ public class FiboC {
 
     public static void main(String[] args) {
         FiboC fibo = new FiboC();
-        int n = 10;
-        int m = 2;
+        long n = 999999999;
+        int m = 321;
         System.out.printf("fasterC(%d)=%d \n\t time=%d \n\n", n, fibo.fasterC(n, m), fibo.time());
     }
 
+    private static long findPeriodPisano(int m) {
+        long a = 0, b = 1, res = 0;
+
+        for (int i = 0; i < m * m; i++) {
+            long c = b;
+            b = (a + b) % m;
+            a = c;
+
+            if (a == 0 && b == 1)
+                res = i + 1;
+        }
+        return res;
+    }
+
     long fasterC(long n, int m) {
-        //Решение сложно найти интуитивно
-        //возможно потребуется дополнительный поиск информации
-        //см. период Пизано
-        return 0L;
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return 1;
+        
+        n = n % findPeriodPisano(m);
+
+        long a = 0, b = 1;
+        for (int i = 0; i < n - 1; i++) {
+            long c;
+            c = b;
+            b = (a + b) % m;
+            a = c;
+        }
+        return b % m;
     }
 
 
