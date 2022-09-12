4b742dce103b82fb659de1cb20e038d5ccf34d2a
yakubsha22888@gmail.com <yakubsha22888@gmail.com>
2022-09-12 17:10:25 +0300

    Lesson1 A B C

--- src/by/it/group151004/haiko/lesson01/FiboC.java
+++ src/by/it/group151004/haiko/lesson01/FiboC.java
@@ -6,6 +6,8 @@ package by.it.group151004.haiko.lesson01;
  * время расчета должно быть не более 2 секунд
  */
 
+import java.math.BigInteger;
+
 public class FiboC {
 
     private long startTime = System.currentTimeMillis();
@@ -25,7 +27,31 @@ public class FiboC {
         //Решение сложно найти интуитивно
         //возможно потребуется дополнительный поиск информации
         //см. период Пизано
-        return 0L;
+        if (m==2){
+            if (n % 3 ==0){
+                return 0;
+            }
+            else return 1;
+        }
+        int[] modRes = new int[m*6+2];
+        BigInteger First=BigInteger.ZERO, Second = BigInteger.ONE, Res = BigInteger.ZERO;
+        modRes[0] = 0; modRes[1] = 1;
+        int counter = 2;
+        boolean timePer=true;
+        while (timePer){
+
+            Res = First.add(Second);
+            First = Second;
+            Second = Res;
+            modRes[counter] = Res.mod(BigInteger.valueOf(m)).intValue();
+            counter++;
+            if(modRes[counter-1]==1 && modRes[counter-2]==0){
+                timePer=false;
+                counter-=2;
+            }
+        }
+        return  modRes[(int)n % counter];
+
     }
 
 
