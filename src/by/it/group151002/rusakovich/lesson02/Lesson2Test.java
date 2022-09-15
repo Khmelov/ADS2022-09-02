c56be74df8c9178e91de67c652366f67ad48e10a
Demian23 <Smegor04@gmail.com>
2022-09-15 21:35:59 +0300

    Remove unnecessary test and merge it and default to one

--- src/by/it/group151002/rusakovich/lesson02/Lesson2Test.java
+++ src/by/it/group151002/rusakovich/lesson02/Lesson2Test.java
@@ -41,23 +41,12 @@ public class Lesson2Test {
         List<B_Sheduler.Event> starts = instance.calcStartTimes(events, 0, 10);  //рассчитаем оптимальное заполнение аудитории
         boolean ok=starts.toString().equals("[(0:1), (1:2), (2:3), (3:5), (6:7), (7:9)]");
         assertTrue("B_Sheduler failed", ok);
-    }
-
-    @Test
-    public void B_Sheduler_f(){
-        B_Sheduler instance = new B_Sheduler();
-        B_Sheduler.Event[] events = {new B_Sheduler.Event(0, 3), new B_Sheduler.Event(0, 1), new B_Sheduler.Event(1, 2), new B_Sheduler.Event(3, 5),
-                new B_Sheduler.Event(1, 3), new B_Sheduler.Event(1, 3), new B_Sheduler.Event(1, 3), new B_Sheduler.Event(3, 6),
-                new B_Sheduler.Event(2, 7), new B_Sheduler.Event(2, 3), new B_Sheduler.Event(2, 7), new B_Sheduler.Event(7, 9),
-                new B_Sheduler.Event(3, 5), new B_Sheduler.Event(2, 4), new B_Sheduler.Event(2, 3), new B_Sheduler.Event(3, 7),
-                new B_Sheduler.Event(4, 5), new B_Sheduler.Event(6, 7), new B_Sheduler.Event(6, 9), new B_Sheduler.Event(7, 9),
-                new B_Sheduler.Event(8, 9), new B_Sheduler.Event(4, 6), new B_Sheduler.Event(8, 10), new B_Sheduler.Event(7, 10)
-        };
-
-        List<B_Sheduler.Event> starts = instance.calcStartTimes(events, 2, 10);  //рассчитаем оптимальное заполнение аудитории
-        boolean ok=starts.toString().equals("[(2:3), (3:5), (6:7), (7:9)]");
+        //shift start border
+        starts = instance.calcStartTimes(events, 2, 10);
+        ok=starts.toString().equals("[(2:3), (3:5), (6:7), (7:9)]");
         assertTrue("B_Sheduler failed", ok);
     }
+
     @Test
     public void C_GreedyKnapsack() throws Exception {
         String root=System.getProperty("user.dir")+"/src/";
