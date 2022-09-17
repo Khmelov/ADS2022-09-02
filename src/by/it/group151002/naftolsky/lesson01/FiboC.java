097a24fda2904aa7ef78084e227651e81333a3ea
Ptuxa <sdnaft@gmail.com>
2022-09-17 12:09:33 +0300

    1

--- src/by/it/group151002/naftolsky/lesson01/FiboC.java
+++ src/by/it/group151002/naftolsky/lesson01/FiboC.java
@@ -16,8 +16,8 @@ public class FiboC {
 
     public static void main(String[] args) {
         FiboC fibo = new FiboC();
-        int n = 10;
-        int m = 2;
+        int n = 13;
+        int m = 5;
         System.out.printf("fasterC(%d)=%d \n\t time=%d \n\n", n, fibo.fasterC(n, m), fibo.time());
     }
 
