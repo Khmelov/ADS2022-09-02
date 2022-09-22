46e0517629b3afa25c6396abea2350d19c936a34
l9sik <hotspot.by@gmail.com>
2022-09-23 00:32:08 +0300

    refactor: use different magic number

--- src/by/it/group151002/poluectov/lesson01/FiboC.java
+++ src/by/it/group151002/poluectov/lesson01/FiboC.java
@@ -18,8 +18,8 @@ public class FiboC {
 
     public static void main(String[] args) {
         FiboC fibo = new FiboC();
-        int n = 10;
-        int m = 2;
+        long n = 999999999;
+        int m = 321;
 
         System.out.printf("fasterC(%d)=%d \n\t time=%d \n\n", n, fibo.fasterC(n, m), fibo.time());
     }
