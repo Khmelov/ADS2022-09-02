31c1de7555313d0be756047d666a65ca2327cd81
FedorDevelopmer <fedor.sap@gmail.com>
2022-09-22 17:58:55 +0300

    Lesson 1 A,B,C Lesson 2 A,B,C

--- src/by/it/group151001/saprankov/lesson02/C_GreedyKnapsack.java
+++ src/by/it/group151001/saprankov/lesson02/C_GreedyKnapsack.java
@@ -43,6 +43,29 @@ public class C_GreedyKnapsack {
         }
     }
 
+    void combsort(Item[] items){
+        int l= items.length;
+        int gap=l;
+        Item temp;
+        double fact = 1.3;
+        while (gap>1){
+            gap /= fact;
+            int i=0;
+            int j=gap;
+            while (j<l){
+                if ((items[i].cost/items[i].weight)<(items[j].cost/items[j].weight)){
+                    temp=items[i];
+                    items[i]=items[j];
+                    items[j]=temp;
+                }
+                i++;
+                j++;
+            }
+        }
+    }
+
+
+
     double calc(File source) throws FileNotFoundException {
         Scanner input = new Scanner(source);
         int n = input.nextInt();      //сколько предметов в файле
@@ -51,6 +74,7 @@ public class C_GreedyKnapsack {
         for (int i = 0; i < n; i++) { //создавая каждый конструктором
             items[i] = new Item(input.nextInt(), input.nextInt());
         }
+        combsort(items);
         //покажем предметы
         for (Item item:items) {
             System.out.println(item);
@@ -58,18 +82,29 @@ public class C_GreedyKnapsack {
         System.out.printf("Всего предметов: %d. Рюкзак вмещает %d кг.\n",n,W);
 
         //тут необходимо реализовать решение задачи
-        //итогом является максимально воможная стоимость вещей в рюкзаке
+        //итогом является максимально возможная стоимость вещей в рюкзаке
         //вещи можно резать на кусочки (непрерывный рюкзак)
         //тут реализуйте алгоритм сбора рюкзака
         //будет особенно хорошо, если с собственной сортировкой
         //кроме того, можете описать свой компаратор в классе Item
         //ваше решение.
         double result = 0;
+        double factor;
+        int i;
+        for(i=0;i<n&&W>0;i++){
+            result+=items[i].cost;
+            W-=items[i].weight;
+        }
+        i--;
+        if(W<0){
+            result-=items[i].cost;
+            W+=items[i].weight;
+            factor=(double)W / (double) items[i].weight;
+            result+=items[i].cost*factor;
+            W-=items[i].weight*factor;
 
 
-
-
-
+        }
 
         System.out.printf("Удалось собрать рюкзак на сумму %f\n",result);
         return result;
