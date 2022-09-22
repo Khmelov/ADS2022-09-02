acfebd30f0055c180d46648a176f7fbcadd35cdf
CinderAle <a.d.strukov@gmail.com>
2022-09-22 22:01:37 +0300

    Lesson 1 ABC

--- src/by/it/group151002/strukov/lesson01/FiboA.java
+++ src/by/it/group151002/strukov/lesson01/FiboA.java
@@ -19,7 +19,7 @@ public class FiboA {
         int n = 33;
         System.out.printf("calc(%d)=%d \n\t time=%d \n\n", n, fibo.calc(n), fibo.time());
 
-        //вычисление чисел фибоначчи медленным методом (рекурсией)
+        //вычисление чисел фибоначчи медленным методом (рекурсией).
         fibo = new FiboA();
         n = 33;
         System.out.printf("slowA(%d)=%d \n\t time=%d \n\n", n, fibo.slowA(n), fibo.time());
