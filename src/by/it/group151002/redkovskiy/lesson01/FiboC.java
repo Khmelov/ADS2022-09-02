c700ab766b895170ab5e1fec4f3bb4f817699243
RomanRedkovskiy <romanredkovskiy10@gmail.com>
2022-09-27 12:10:14 +0300

    lesson 1 and lesson 2A

--- src/by/it/group151002/redkovskiy/lesson01/FiboC.java
+++ src/by/it/group151002/redkovskiy/lesson01/FiboC.java
@@ -1,5 +1,4 @@
 package by.it.group151002.redkovskiy.lesson01;
-
 /*
  * Даны целые числа 1<=n<=1E18 и 2<=m<=1E5,
  * необходимо найти остаток от деления n-го числа Фибоначчи на m.
@@ -7,7 +6,8 @@ package by.it.group151002.redkovskiy.lesson01;
  */
 
 public class FiboC {
-
+    public static final int MAX_PIZANO_PERIOD = 6;
+    //период Пизано числа m может быть не больше чем в 6 раз больше самого числа m
     private long startTime = System.currentTimeMillis();
 
     private long time() {
@@ -22,10 +22,23 @@ public class FiboC {
     }
 
     long fasterC(long n, int m) {
-        //Решение сложно найти интуитивно
-        //возможно потребуется дополнительный поиск информации
-        //см. период Пизано
-        return 0L;
+        int i = 1;
+        int position;
+        long multiplier;
+        long[] arr = new long[MAX_PIZANO_PERIOD*m];
+        boolean isPeriodFound = false;
+        if(n <= 1)
+            return n % m;
+        arr[0] = 0; arr[1] = 1;
+        while (!isPeriodFound && (i < n)){
+            i++;
+            arr[i] = arr[i-1] + arr[i-2];
+            if(arr[i-1] % m == 0 && arr[i] % m == 1)
+                isPeriodFound = true;
+        }
+        multiplier = n / (i-1);
+        position = (int)(n - multiplier*(i-1));
+        return arr[position] % m ;
     }
 
 
