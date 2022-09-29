d6d7e0a0d8a2870a357580dc78c9d2665e77f4ca
fatalryuu <vanyax941@gmail.com>
2022-09-29 19:01:27 +0300

    Lesson 3 A

---
+++ src/by/it/group151002/shatko/lesson03/A_Huffman.java
@@ -0,0 +1,153 @@
+package by.it.group151002.shatko.lesson03;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+//Lesson 3. A_Huffman.
+//Разработайте метод encode(File file) для кодирования строки (код Хаффмана)
+
+// По данным файла (непустой строке ss длины не более 104104),
+// состоящей из строчных букв латинского алфавита,
+// постройте оптимальный по суммарной длине беспрефиксный код.
+
+// Используйте Алгоритм Хаффмана — жадный алгоритм оптимального
+// безпрефиксного кодирования алфавита с минимальной избыточностью.
+
+// В первой строке выведите количество различных букв kk,
+// встречающихся в строке, и размер получившейся закодированной строки.
+// В следующих kk строках запишите коды букв в формате "letter: code".
+// В последней строке выведите закодированную строку. Примеры ниже
+
+//        Sample Input 1:
+//        a
+//
+//        Sample Output 1:
+//        1 1
+//        a: 0
+//        0
+
+//        Sample Input 2:
+//        abacabad
+//
+//        Sample Output 2:
+//        4 14
+//        a: 0
+//        b: 10
+//        c: 110
+//        d: 111
+//        00101100100111
+
+public class A_Huffman {
+
+    abstract class Node implements Comparable<Node> {
+        private final int frequence;
+
+        abstract void fillCodes(String code);
+
+        private Node(int frequence) {
+            this.frequence = frequence;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(frequence, o.frequence);
+        }
+    }
+
+    private class InternalNode extends Node {
+        Node left;
+        Node right;
+
+        InternalNode(Node left, Node right) {
+            super(left.frequence + right.frequence);
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        void fillCodes(String code) {
+            left.fillCodes(code + "0");
+            right.fillCodes(code + "1");
+        }
+
+    }
+
+    private class LeafNode extends Node {
+        char symbol;
+
+        LeafNode(int frequence, char symbol) {
+            super(frequence);
+            this.symbol = symbol;
+        }
+
+        @Override
+        void fillCodes(String code) {
+            codes.put(this.symbol, code);
+        }
+    }
+
+    static private Map<Character, String> codes = new TreeMap<>();
+
+    String encode(File file) throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+        String s = scanner.next();
+        scanner.close();
+        char[] sArr = s.toCharArray();
+        Map<Character, Integer> count = new HashMap<>();
+
+        for (char c : sArr) {
+            if (count.containsKey(c)) {
+                count.put(c, count.get(c) + 1);
+            } else {
+                count.put(c, 1);
+            }
+        }
+
+        PriorityQueue<Node> priorityQueue = new PriorityQueue<>();
+        int i = 0;
+        while (i < sArr.length) {
+            if (count.containsKey(sArr[i])) {
+                LeafNode currNode = new LeafNode(count.get(sArr[i]), sArr[i]);
+                priorityQueue.add(currNode);
+                count.remove(sArr[i]);
+            }
+            i++;
+        }
+
+        int lastNum = priorityQueue.size() - 1;
+        for (i = 0; i < lastNum; i++) {
+            Node left = priorityQueue.poll();
+            Node right = priorityQueue.poll();
+            assert left != null;
+            assert right != null;
+            InternalNode parentNode = new InternalNode(left, right);
+            priorityQueue.add(parentNode);
+        }
+
+        StringBuilder encryptedString = new StringBuilder();
+        Node root = priorityQueue.poll();
+        assert root != null;
+        root.fillCodes("");
+        for (i = 0; i < sArr.length; i++) {
+            encryptedString.append(codes.get(sArr[i]));
+        }
+
+        return encryptedString.toString();
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String root = System.getProperty("user.dir") + "/src/";
+        File f = new File(root + "by/it/a_khmelev/lesson03/dataHuffman.txt");
+        A_Huffman instance = new A_Huffman();
+        long startTime = System.currentTimeMillis();
+        String result = instance.encode(f);
+        long finishTime = System.currentTimeMillis();
+        System.out.printf("%d %d\n", codes.size(), result.length());
+        for (Map.Entry<Character, String> entry : codes.entrySet()) {
+            System.out.printf("%s: %s\n", entry.getKey(), entry.getValue());
+        }
+        System.out.println(result);
+    }
+
+}
