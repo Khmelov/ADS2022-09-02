03c800c6cdcbb5a9876e43c8346d9cd53a32fd27
fatalryuu <vanyax941@gmail.com>
2022-09-30 11:18:00 +0300

    Lesson 3 C solution

--- src/by/it/group151002/shatko/lesson03/C_HeapMax.java
+++ src/by/it/group151002/shatko/lesson03/C_HeapMax.java
@@ -3,34 +3,6 @@ package by.it.group151002.shatko.lesson03;
 import java.io.*;
 import java.util.*;
 
-// Lesson 3. C_Heap.
-// Задача: построить max-кучу = пирамиду = бинарное сбалансированное дерево на массиве.
-// ВАЖНО! НЕЛЬЗЯ ИСПОЛЬЗОВАТЬ НИКАКИЕ КОЛЛЕКЦИИ, КРОМЕ ARRAYLIST (его можно, но только для массива)
-
-//      Проверка проводится по данным файла
-//      Первая строка входа содержит число операций 1 ≤ n ≤ 100000.
-//      Каждая из последующих nn строк задают операцию одного из следующих двух типов:
-
-//      Insert x, где 0 ≤ x ≤ 1000000000 — целое число;
-//      ExtractMax.
-
-//      Первая операция добавляет число x в очередь с приоритетами,
-//      вторая — извлекает максимальное число и выводит его.
-
-//      Sample Input:
-//      6
-//      Insert 200
-//      Insert 10
-//      ExtractMax
-//      Insert 5
-//      Insert 500
-//      ExtractMax
-//
-//      Sample Output:
-//      200
-//      500
-
-
 public class C_HeapMax {
     private class MaxHeap {
         private List<Long> heap = new ArrayList<>();
@@ -40,7 +12,7 @@ public class C_HeapMax {
                 int left = 2 * i + 1;
                 int right = 2 * i + 2;
                 int j = left;
-                if (right < heap.size() && heap.get(right) < heap.get(left))
+                if (right < heap.size() && heap.get(right) > heap.get(left))
                     j = right;
                 if (heap.get(i) >= heap.get(j))
                     break;
@@ -53,17 +25,25 @@ public class C_HeapMax {
         }
 
         int siftUp(int i) { //просеивание вверх
-
+            while (heap.get(i) > heap.get((i - 1) / 2)) {
+                long tmp = heap.get(i);
+                heap.set(i, heap.get((i - 1) / 2));
+                heap.set((i - 1) / 2, tmp);
+                i = (i - 1) / 2;
+            }
             return i;
         }
 
         void insert(Long value) { //вставка
-
+            heap.add(value);
+            siftUp(heap.size() - 1);
         }
 
         Long extractMax() { //извлечение и удаление максимума
-            Long result = null;
-
+            Long result = heap.get(0);
+            heap.set(0, heap.get(heap.size() - 1));
+            heap.remove(heap.size() - 1);
+            siftDown(0);
             return result;
         }
     }
