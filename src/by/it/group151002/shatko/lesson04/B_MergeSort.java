6c29db6068a6c4607882cacefbcbee099c2fe3ca
fatalryuu <vanyax941@gmail.com>
2022-10-01 21:46:14 +0300

    Lesson 4 B

--- src/by/it/group151002/shatko/lesson04/B_MergeSort.java
+++ src/by/it/group151002/shatko/lesson04/B_MergeSort.java
@@ -5,56 +5,69 @@ import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.Scanner;
 
-/*
-Реализуйте сортировку слиянием для одномерного массива.
-Сложность алгоритма должна быть не хуже, чем O(n log n)
-
-Первая строка содержит число 1<=n<=10000,
-вторая - массив A[1…n], содержащий натуральные числа, не превосходящие 10E9.
-Необходимо отсортировать полученный массив.
-
-Sample Input:
-5
-2 3 9 2 9
-Sample Output:
-2 2 3 9 9
-*/
 public class B_MergeSort {
 
-    int[] getMergeSort(InputStream stream) throws FileNotFoundException {
-        //подготовка к чтению данных
-        Scanner scanner = new Scanner(stream);
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-
-        //размер массива
-        int n = scanner.nextInt();
-        //сам массив
-        int[] a=new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = scanner.nextInt();
-            System.out.println(a[i]);
+    void merge(int[] arr, int[] l, int[] r, int left, int right) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < left && j < right) {
+            if (l[i] <= r[j]) {
+                arr[k++] = l[i++];
+            } else {
+                arr[k++] = r[j++];
+            }
         }
 
-        // тут ваше решение (реализуйте сортировку слиянием)
-        // https://ru.wikipedia.org/wiki/Сортировка_слиянием
+        while (i < left) {
+            arr[k++] = l[i++];
+        }
 
+        while (j < right) {
+            arr[k++] = r[j++];
+        }
+    }
 
+    void mergeSort(int[] arr, int n) {
+        if (n == 1) return;
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
 
+        for (int i = 0; i < mid; i++) {
+            l[i] = arr[i];
+        }
 
+        for (int i = mid; i < n; i++) {
+            r[i - mid] = arr[i];
+        }
 
+        mergeSort(l, mid);
+        mergeSort(r, n - mid);
 
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-        return a;
+        merge(arr, l, r, mid, n - mid);
+    }
+
+    int[] getMergeSort(InputStream stream) throws FileNotFoundException {
+        Scanner scanner = new Scanner(stream);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("\n");
+        mergeSort(arr, n);
+
+        return arr;
     }
     public static void main(String[] args) throws FileNotFoundException {
         String root = System.getProperty("user.dir") + "/src/";
-        InputStream stream = new FileInputStream(root + "by/it/a_khmelev/lesson04/dataB.txt");
+        InputStream stream = new FileInputStream(root + "by/it/group151002/shatko/lesson04/dataB.txt");
         B_MergeSort instance = new B_MergeSort();
-        //long startTime = System.currentTimeMillis();
-        int[] result=instance.getMergeSort(stream);
-        //long finishTime = System.currentTimeMillis();
+        int[] result = instance.getMergeSort(stream);
         for (int index:result){
-            System.out.print(index+" ");
+            System.out.print(index + " ");
         }
     }
 
