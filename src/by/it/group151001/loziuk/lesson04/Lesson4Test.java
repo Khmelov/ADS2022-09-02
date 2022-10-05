ae84daa1512511fe32881a5ce14b17ff0dea0f4f
loziuk <loziukvika10@gmail.com>
2022-10-05 21:10:33 +0300

    lesson4A

--- src/by/it/group151001/loziuk/lesson04/Lesson4Test.java
+++ src/by/it/group151001/loziuk/lesson04/Lesson4Test.java
@@ -12,7 +12,7 @@ public class Lesson4Test {
     @Test
     public void A() throws Exception {
         String root = System.getProperty("user.dir") + "/src/";
-        InputStream stream = new FileInputStream(root + "by/it/a_khmelov/lesson04/dataA.txt");
+        InputStream stream = new FileInputStream(root + "by/it/a_khmelev/lesson04/dataA.txt");
         A_BinaryFind instance = new A_BinaryFind();
         //long startTime = System.currentTimeMillis();
         int[] result=instance.findIndex(stream);
