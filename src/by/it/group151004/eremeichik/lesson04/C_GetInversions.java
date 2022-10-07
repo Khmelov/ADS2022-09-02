fefa6289dbccfcd18ab2a76a8aea77c0d23ec591
hahahalol9 <cmak7543@mail.ru>
2022-10-07 10:59:03 +0300

    add lesson 3,4 C

--- src/by/it/group151004/eremeichik/lesson04/C_GetInversions.java
+++ src/by/it/group151004/eremeichik/lesson04/C_GetInversions.java
@@ -3,6 +3,7 @@ package by.it.group151004.eremeichik.lesson04;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /*
@@ -46,19 +47,45 @@ public class C_GetInversions {
         for (int i = 0; i < n; i++) {
             a[i] = scanner.nextInt();
         }
-        int result = 0;
-        //!!!!!!!!!!!!!!!!!!!!!!!!     тут ваше решение   !!!!!!!!!!!!!!!!!!!!!!!!
 
+        return mergeSortAndCount(a,0,a.length-1);
+    }
 
+    private static int mergeAndCount(int[] arr, int l,
+                                     int m, int r)
+    {
+        int[] left = Arrays.copyOfRange(arr, l, m + 1);
+        int[] right = Arrays.copyOfRange(arr, m + 1, r + 1);
+        int i = 0, j = 0, k = l, swaps = 0;
+        while (i < left.length && j < right.length)
+        {
+            if (left[i] <= right[j])
+                arr[k++] = left[i++];
+            else
+            {
+                arr[k++] = right[j++];
+                swaps += (m + 1) - (l + i);
+            }
+        }
+        while (i < left.length)
+            arr[k++] = left[i++];
+        while (j < right.length)
+            arr[k++] = right[j++];
+        return swaps;
+    }
 
+    private static int mergeSortAndCount(int[] arr,
+                                         int l, int r)
+    {
+        int count = 0;
+        if (l < r) {
+            int m = (l + r) / 2;
+            count += mergeSortAndCount(arr, l, m);
+            count += mergeSortAndCount(arr, m + 1, r);
+            count += mergeAndCount(arr, l, m, r);
+        }
 
-
-
-
-
-
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-        return result;
+        return count;
     }
 
 
