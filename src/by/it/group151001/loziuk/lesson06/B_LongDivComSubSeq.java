7311a49588a45ee5de96da9305bab6ade8f213b0
loziuk <loziukvika10@gmail.com>
2022-10-22 02:16:33 +0300

    Lesson6

--- src/by/it/group151001/loziuk/lesson06/B_LongDivComSubSeq.java
+++ src/by/it/group151001/loziuk/lesson06/B_LongDivComSubSeq.java
@@ -43,7 +43,19 @@ public class B_LongDivComSubSeq {
             m[i] = scanner.nextInt();
         }
         //тут реализуйте логику задачи методами динамического программирования (!!!)
-        int result = 0;
+        int[] d = new int[n];
+        for (int i = 0;i < n; i++){
+            d[i] = 1;
+            for (int j = 0; j < i; j++){
+                if (m[i] % m[j] == 0 && d[i] < d[j] + 1)
+                    d[i] = d[j] + 1;
+            }
+        }
+        int result = d[0];
+        for (int i = 1; i < n; i++){
+            if (d[i] > result)
+                result = d[i];
+        }
 
 
         //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
