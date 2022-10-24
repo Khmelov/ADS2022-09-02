a29ec6e0b50f02bbfea82dab2970b4f7c49a1513
CinderAle <a.d.strukov@gmail.com>
2022-10-25 00:35:26 +0300

    Lessons 3, 4 Full

--- src/by/it/group151002/strukov/lesson04/C_GetInversions.java
+++ src/by/it/group151002/strukov/lesson04/C_GetInversions.java
@@ -35,6 +35,49 @@ Sample Output:
 
 public class C_GetInversions {
 
+    public int invertions = 0;
+    void merge(int[] a, int p, int q, int r) {
+        int n1 = q - p + 1;
+        int n2 = r - q;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        int i, j, k;
+        for (i = 0; i < n1; i++) {
+            L[i] = a[p + i];
+        }
+
+        for (j = 0; j < n2; j++) {
+            R[j] = a[q + 1 + j];
+        }
+
+        i = 0;
+        j = 0;
+        k = p;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                a[k++] = L[i++];
+            }
+            else {
+                a[k++] = R[j++];
+                invertions = invertions + q + 1 - p - i;
+            }
+        }
+        while (i < n1) {
+            a[k++] = L[i++];
+        }
+        while (j < n2) {
+            a[k++] = R[j++];
+        }
+    }
+    void mergeSort(int[] a, int p, int r) {
+        if (p < r) {
+            int q = (p + r) / 2;
+            mergeSort(a, p, q);
+            mergeSort(a, q + 1, r);
+            merge(a, p, q, r);
+        }
+    }
+
     int calc(InputStream stream) throws FileNotFoundException {
         //подготовка к чтению данных
         Scanner scanner = new Scanner(stream);
@@ -51,14 +94,14 @@ public class C_GetInversions {
 
 
 
-
+        mergeSort(a, 0, n - 1);
 
 
 
 
 
         //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-        return result;
+        return invertions;
     }
 
 
