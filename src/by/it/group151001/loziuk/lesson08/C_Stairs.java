a176e7a9163348b7188dd8480a044f4702bc7a34
loziuk <loziukvika10@gmail.com>
2022-10-29 18:08:48 +0300

    Lesson8C

--- src/by/it/group151001/loziuk/lesson08/C_Stairs.java
+++ src/by/it/group151001/loziuk/lesson08/C_Stairs.java
@@ -42,11 +42,13 @@ public class C_Stairs {
             stairs[i]=scanner.nextInt();
         }
         //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-        int result = 0;
+        int[] sum = new int[n + 1];
+        sum[0] = 0;
+        sum[1] = stairs[0];
+        for (int i = 2;i <= n; i++)
+            sum[i] = Math.max(stairs[i - 1] + sum[i - 1],stairs[i - 1] + sum[i - 2]);
 
-
-
-
+        int result = sum[n];
         //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
         return result;
     }
