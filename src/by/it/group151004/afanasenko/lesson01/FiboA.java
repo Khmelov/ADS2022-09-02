b4d85bcfe944aa033bccc55a63651698a889902d
KornetBy <KornetBy@gmail.com>
2022-11-06 22:27:32 +0300

    lesson1

--- src/by/it/group151004/afanasenko/lesson01/FiboA.java
+++ src/by/it/group151004/afanasenko/lesson01/FiboA.java
@@ -2,15 +2,12 @@ package by.it.group151004.afanasenko.lesson01;
 
 import java.math.BigInteger;
 
-/*
- * Вам необходимо выполнить рекурсивный способ вычисления чисел Фибоначчи
- */
+import java.math.BigInteger;
 
 public class FiboA {
+    public long startTime = System.currentTimeMillis();
 
-    private long startTime = System.currentTimeMillis();
-
-    private long time() {
+    public long time() {
         return System.currentTimeMillis() - startTime;
     }
 
@@ -19,30 +16,25 @@ public class FiboA {
         int n = 33;
         System.out.printf("calc(%d)=%d \n\t time=%d \n\n", n, fibo.calc(n), fibo.time());
 
-        //вычисление чисел фибоначчи медленным методом (рекурсией)
         fibo = new FiboA();
         n = 33;
         System.out.printf("slowA(%d)=%d \n\t time=%d \n\n", n, fibo.slowA(n), fibo.time());
     }
 
-
-    private int calc(int n) {
-        //здесь простейший вариант, в котором код совпадает с мат.определением чисел Фибоначчи
-        //время O(2^n)
-        return 0;
+    public int calc(int n) {
+        if (n < 2) {
+            return n;
+        }
+        return calc(n - 1) + calc(n - 2);
     }
 
-
-    BigInteger slowA(Integer n) {
-        //рекурсия
-        //здесь нужно реализовать вариант без ограничения на размер числа,
-        //в котором код совпадает с мат.определением чисел Фибоначчи
-        //время O(2^n)
-
-        return BigInteger.ZERO;
+    public BigInteger slowA(Integer n) {
+        switch (n) {
+            case 0:
+                return BigInteger.ZERO;
+            case 1:
+                return BigInteger.ONE;
+        }
+        return slowA(n - 1).add(slowA(n - 2));
     }
-
-
-
 }
-
--- src/by/it/group151004/afanasenko/lesson01/FiboC.java
+++ src/by/it/group151004/afanasenko/lesson01/FiboC.java
@@ -7,27 +7,58 @@ package by.it.group151004.afanasenko.lesson01;
  */
 
 public class FiboC {
+    public long startTime = System.currentTimeMillis();
 
-    private long startTime = System.currentTimeMillis();
-
-    private long time() {
+    public long time() {
         return System.currentTimeMillis() - startTime;
     }
 
     public static void main(String[] args) {
         FiboC fibo = new FiboC();
-        int n = 10;
-        int m = 2;
+        long n = 999999999;
+        int m = 321;
         System.out.printf("fasterC(%d)=%d \n\t time=%d \n\n", n, fibo.fasterC(n, m), fibo.time());
     }
+    public long fasterC(long n, int m) {
+        if (n == 1) {
+            return 1L;
+        } else {
+            long curr = 1;
+            long prev = 1;
 
-    long fasterC(long n, int m) {
-        //Решение сложно найти интуитивно
-        //возможно потребуется дополнительный поиск информации
-        //см. период Пизано
-        return 0L;
+            int i = 2;
+            long a0 = 0L;
+            long a1 = 1L;
+            long an = 1L;
+            long length = 1;
+            boolean isFound = false;
+
+            while (!isFound) {
+                prev = curr;
+                an = a0 + a1;
+                curr = an % m;
+                a0 = a1;
+                a1 = an;
+                if (curr == 1 && prev == 0) {
+                    isFound = true;
+                }
+                i++;
+                length++;
+            }
+
+            length--;
+            n = n % length;
+            a0 = 0L;
+            a1 = 1L;
+            an = 1L;
+            for (i = 2; i <= n; i++) {
+                an = a0 + a1;
+                a0 = a1;
+                a1 = an;
+            }
+
+            return an % m;
+        }
     }
-
-
 }
 
