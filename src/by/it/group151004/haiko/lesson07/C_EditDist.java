9860b4e3b7e6150e11be3840e17668355e0f9d95
yakubsha22888@gmail.com <yakubsha22888@gmail.com>
2022-11-07 23:08:45 +0300

    lesson 6 (ABC) 7 (AB) completed

---
+++ src/by/it/group151004/haiko/lesson07/C_EditDist.java
@@ -0,0 +1,88 @@
+package by.it.group151004.haiko.lesson07;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+/*
+Задача на программирование: расстояние Левенштейна
+    https://ru.wikipedia.org/wiki/Расстояние_Левенштейна
+    http://planetcalc.ru/1721/
+
+Дано:
+    Две данных непустые строки длины не более 100, содержащие строчные буквы латинского алфавита.
+
+Необходимо:
+    Решить задачу МЕТОДАМИ ДИНАМИЧЕСКОГО ПРОГРАММИРОВАНИЯ
+    Итерационно вычислить алгоритм преобразования двух данных непустых строк
+    Вывести через запятую редакционное предписание в формате:
+     операция("+" вставка, "-" удаление, "~" замена, "#" копирование)
+     символ замены или вставки
+
+    Sample Input 1:
+    ab
+    ab
+    Sample Output 1:
+    #,#,
+
+    Sample Input 2:
+    short
+    ports
+    Sample Output 2:
+    -s,~p,#,#,#,+s,
+
+    Sample Input 3:
+    distance
+    editing
+    Sample Output 2:
+    +e,#,#,-s,#,~i,#,-c,~g,
+
+
+    P.S. В литературе обычно действия редакционных предписаний обозначаются так:
+    - D (англ. delete) — удалить,
+    + I (англ. insert) — вставить,
+    ~ R (replace) — заменить,
+    # M (match) — совпадение.
+*/
+
+
+public class C_EditDist {
+
+    String getDistanceEdinting(String one, String two) {
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        int n = one.length(), m = two.length();
+        int[][] T = new int[one.length()+1][two.length()+1];
+        for (int i=0; i<= n; i++){
+            T[i][0]=i;
+        }
+        for (int i=0; i<=m; i++){
+            T[0][i]=i;
+        }
+        for (int i=1; i<=n; i++){
+            for (int j=1; j<=m; j++){
+                int diff = 0;
+                if (one.charAt(i-1) != two.charAt(j-1)) diff=1;
+                T[i][j] = Integer.min(T[i-1][j]+1, Integer.min(T[i][j-1]+1, (T[i-1][j-1]+diff) ) );
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+
+
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        return result.toString();
+    }
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String root = System.getProperty("user.dir") + "/src/";
+        InputStream stream = new FileInputStream(root + "by/it/a_khmelev/lesson07/dataABC.txt");
+        C_EditDist instance = new C_EditDist();
+        Scanner scanner = new Scanner(stream);
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(),scanner.nextLine()));
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(),scanner.nextLine()));
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(),scanner.nextLine()));
+    }
+
+}
