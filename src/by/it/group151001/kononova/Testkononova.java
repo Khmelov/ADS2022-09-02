ad542e2cc077674611c84fcfc3f8d67b01e982c0
DanaKononova <dana.kononova@mail.ru>
2022-11-08 16:38:27 +0300

    Lesson 7 A, B, C

--- src/by/it/group151001/kononova/Testkononova.java
+++ src/by/it/group151001/kononova/Testkononova.java
@@ -18,6 +18,9 @@ import by.it.group151001.kononova.lesson05.C_QSortOptimized;
 import by.it.group151001.kononova.lesson06.A_LIS;
 import by.it.group151001.kononova.lesson06.B_LongDivComSubSeq;
 import by.it.group151001.kononova.lesson06.C_LongNotUpSubSeq;
+import by.it.group151001.kononova.lesson07.A_EditDist;
+import by.it.group151001.kononova.lesson07.B_EditDist;
+import by.it.group151001.kononova.lesson07.C_EditDist;
 import org.junit.Test;
 
 import java.io.File;
@@ -27,6 +30,7 @@ import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @SuppressWarnings("all")
@@ -313,6 +317,38 @@ public class Testkononova {
         int result=instance.getNotUpSeqSize(stream);
         boolean ok=(result==4);
         assertTrue("C failed", ok);
+    }
+
+    @Test
+    public void A7() throws Exception {
+        A_EditDist instance = new A_EditDist();
+        assertEquals("A1 failed", instance.getDistanceEdinting("ab","ab"),0);
+        assertEquals("A2 failed", instance.getDistanceEdinting("short","ports"),3);
+        assertEquals("A3 failed", instance.getDistanceEdinting("distance","editing"),5);
+    }
+
+
+    @Test
+    public void B7() throws Exception {
+        B_EditDist instance = new B_EditDist();
+        assertEquals("B1 failed", instance.getDistanceEdinting("ab","ab"),0);
+        assertEquals("B2 failed", instance.getDistanceEdinting("short","ports"),3);
+        assertEquals("B3 failed", instance.getDistanceEdinting("distance","editing"),5);
+    }
+
+    @Test
+    public void C7() throws Exception {
+        C_EditDist instance = new C_EditDist();
+        assertEquals("C1 failed", instance.getDistanceEdinting("ab","ab"),"#,#,");
+        //путей может быть много, поэтому тут жестко проверить все сложно
+        //надо найти и проверить их все, что делает тест сложнее реализации
+        //возможно, что хватит только подсчета повторов.
+
+        //ожидается     -s,~p,#,#,#,+s,
+        assertEquals("C2 failed", instance.getDistanceEdinting("short","ports").split("#").length,4);
+
+        //ожидается     +e,#,#,-s,#,~i,#,-c,~g,
+        assertEquals("C3 failed", instance.getDistanceEdinting("distance","editing").split("#").length,5);
     }
 
 }
