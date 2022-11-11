aa6f779306fd1710017fdae395a4c462bb98500c
freezton <vlad.freezzy@gmail.com>
2022-11-11 10:47:54 +0300

    lesson 6

--- src/by/it/group151002/protchenko/lesson05/C_QSortOptimized.java
+++ src/by/it/group151002/protchenko/lesson05/C_QSortOptimized.java
@@ -33,7 +33,7 @@ import java.util.Scanner;
 public class C_QSortOptimized {
 
     //отрезок
-    private class Segment  implements Comparable<Segment>{
+    private class Segment implements Comparable<Segment>{
         int start;
         int stop;
 
