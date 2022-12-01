905561a16b4c46cd9727b9db0af3f657241faac2
blessedboy14 <92112019+blessedboy14@users.noreply.github.com>
2022-12-01 20:05:06 +0300

    lesson 10 a,b

--- src/by/it/group151004/prokopchuk/lesson10/TaskB.java
+++ src/by/it/group151004/prokopchuk/lesson10/TaskB.java
@@ -4,63 +4,131 @@ import java.util.*;
 
 public class TaskB<E>  implements NavigableSet<E> {
 
+    private int BASE_SIZE = 10;
+    private Object[] array = new Object[BASE_SIZE];
+    int currSize = 0;
+
     //Создайте БЕЗ использования других классов (включая абстрактные)
     //аналог дерева TreeSet
+
+    private int compareTo(E first, E second) {
+        return Integer.compare((int)first, (int)second);
+    }
 
     //Обязательные к реализации методы и конструкторы
     public TaskB() {
     }
 
+    private void sorting() {
+        Object[] arr = array;
+        for (int i = 0; i < currSize; i++) {
+            for (int j = 0; j < currSize - i - 1; j++) {
+                if (compareTo((E)array[j], (E)array[j+1]) > 0) {
+                    E temp=(E)array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+    }
+
     @Override
     public boolean add(E e) {
-        return false;
+        if (!contains(e)) {
+            if (array.length == 0 || currSize == array.length - 1) {
+                Object[] new_arr = new Object[array.length + BASE_SIZE / 2];
+                System.arraycopy(array, 0, new_arr, 0, currSize);
+                array = new_arr;
+            }
+            array[currSize++] = e;
+            if (currSize > 1) {
+                sorting();
+            }
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public boolean remove(Object o) {
-        return false;
+        int remove = -1;
+        for (int i = 0; i < currSize; i++) {
+            if (array[i] == o) {
+                remove = i;
+                break;
+            }
+        }
+        if (remove == -1)
+            return false;
+        for (int i = remove; i < currSize; i++) {
+            array[i] = array[i+1];
+        }
+        array[currSize] = null;
+        currSize--;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        str.append("[");
+        for (int i = 0; i < currSize; i++) {
+            str.append(array[i]);
+            str.append(", ");
+        }
+        if (str.length() < 2) {
+            str.append("]");
+        } else {
+            str.delete(str.length()-2, str.length());
+            str.append("]");
+        }
+        return str.toString();
     }
 
     @Override
     public boolean contains(Object o) {
+        for (Object el : array) {
+            if (el == o ){
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        Iterator<E> iterator = (Iterator<E>) Arrays.stream(array).iterator();
+        return iterator;
     }
 
     @Override
     public void clear() {
-
+        currSize = 0;
+        array = new Object[currSize];
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return currSize == 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return currSize;
     }
 
     @Override
     public E first() {
-        return null;
+        return (E)array[0];
     }
 
     @Override
     public E last() {
-        return null;
+        return (E)array[currSize-1];
     }
 
 
-    @Override
-    public String toString() {
-        return null;
-    }
     /////////////////////////////////////////////////////////////////////////
     /////////////////////////////////////////////////////////////////////////
     ////////         Эти методы реализовывать необязательно      ////////////
