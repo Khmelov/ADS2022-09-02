f3b6b0d759178e1cedaf2e0cbabdb026623fdddc
Artyom Belsky <artyombel2003@gmail.com>
2022-12-09 21:22:01 +0300

    Started lesson 11.

---
+++ src/by/it/group151004/belsky/lesson11/Graph.java
@@ -0,0 +1,102 @@
+package by.it.group151004.belsky.lesson11;
+
+import java.util.ArrayList;
+
+class Edge {
+    private Vertex from;
+    private Vertex to;
+    private float weight;
+
+    public Edge(Vertex from, Vertex to, float weight) {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+    }
+
+    public Vertex getFrom() {
+        return from;
+    }
+
+    public void setFrom(Vertex from) {
+        this.from = from;
+    }
+
+    public Vertex getTo() {
+        return to;
+    }
+
+    public void setTo(Vertex to) {
+        this.to = to;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+}
+
+class Vertex {
+    private String name;
+    private ArrayList<Edge> outEdges;
+
+    public Vertex(String name) {
+        this.name = name;
+        outEdges = new ArrayList<>();
+    }
+
+    public Vertex(String name, ArrayList<Edge> outEdges) {
+        this.name = name;
+        this.outEdges = outEdges;
+    }
+
+    public void addEdge(Edge e) {
+        outEdges.add(e);
+    }
+
+    public void removeEdge(Edge e) {
+        outEdges.remove(e);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Edge> getOutEdges() {
+        return outEdges;
+    }
+}
+
+public class Graph {
+    ArrayList<Vertex> vertices;
+
+    public Graph() {
+        this.vertices = new ArrayList<>();
+    }
+
+    public Graph(ArrayList<Vertex> vertices) {
+        this.vertices = vertices;
+    }
+
+    public void addVertex(Vertex v) {
+        vertices.add(v);
+    }
+
+    public void removeVertex(Vertex v) {
+        vertices.remove(v);
+    }
+
+    public int size() {
+        return vertices.size();
+    }
+
+    public int indexOf(Vertex v) {
+        return vertices.indexOf(v);
+    }
+}
---
+++ src/by/it/group151004/belsky/lesson11/TaskA.java
@@ -0,0 +1,93 @@
+package by.it.group151004.belsky.lesson11;
+
+import java.util.*;
+
+public class TaskA {
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+        Vertex vA = new Vertex("A");
+        graph.addVertex(vA);
+        Vertex vB = new Vertex("B");
+        graph.addVertex(vB);
+        Vertex vC = new Vertex("C");
+        graph.addVertex(vC);
+        Vertex vD = new Vertex("D");
+        graph.addVertex(vD);
+        Vertex vE = new Vertex("E");
+        graph.addVertex(vE);
+        Vertex vF = new Vertex("F");
+        graph.addVertex(vF);
+        Vertex vG = new Vertex("G");
+        graph.addVertex(vG);
+        Vertex vH = new Vertex("H");
+        graph.addVertex(vH);
+
+        vA.addEdge(new Edge(vA, vB, 1));
+        vA.addEdge(new Edge(vA, vF, 8));
+        vA.addEdge(new Edge(vA, vE, 4));
+
+        vB.addEdge(new Edge(vB, vC, 2));
+        vB.addEdge(new Edge(vB, vG, 6));
+        vB.addEdge(new Edge(vB, vF, 6));
+
+        vC.addEdge(new Edge(vC, vD, 1));
+        vC.addEdge(new Edge(vC, vG, 2));
+
+        vD.addEdge(new Edge(vD, vG, 1));
+        vD.addEdge(new Edge(vD, vH, 4));
+
+        vE.addEdge(new Edge(vE, vF, 5));
+
+        vG.addEdge(new Edge(vG, vF, 1));
+        vG.addEdge(new Edge(vG, vH, 1));
+
+
+        Vertex startV = vA;
+
+        HashMap<Vertex, Float> map = new HashMap<>();
+        map.put(vA, Float.POSITIVE_INFINITY);
+        map.put(vB, Float.POSITIVE_INFINITY);
+        map.put(vC, Float.POSITIVE_INFINITY);
+        map.put(vD, Float.POSITIVE_INFINITY);
+        map.put(vE, Float.POSITIVE_INFINITY);
+        map.put(vF, Float.POSITIVE_INFINITY);
+        map.put(vG, Float.POSITIVE_INFINITY);
+        map.put(vH, Float.POSITIVE_INFINITY);
+        map.put(vA, 0.0f);
+        PriorityQueue<Vertex> pQueue = new PriorityQueue<>(11, (e1, e2) -> (int) Math.floor(map.get(e1) - map.get(e2)));
+        pQueue.add(vA);
+
+        while (!pQueue.isEmpty()) {
+            Vertex prev = pQueue.remove();
+            for (Edge e : prev.getOutEdges()) {
+                if (e.getWeight()+map.get(prev) < map.get(e.getTo())) {
+                    map.put(e.getTo(), e.getWeight()+map.get(prev));
+                    pQueue.add(e.getTo());
+                }
+            }
+        }
+
+        map.forEach(((vertex, aFloat) -> {
+            System.out.println(vertex.getName()+": "+aFloat);
+        }));
+
+    }
+}
+
+class PQueueElement {
+    private final Vertex vertex;
+    private final float qWeight;
+
+    public PQueueElement(Vertex vertex, float qWeight) {
+        this.vertex = vertex;
+        this.qWeight = qWeight;
+    }
+
+    public Vertex getVertex() {
+        return vertex;
+    }
+
+    public float getqWeight() {
+        return qWeight;
+    }
+}
