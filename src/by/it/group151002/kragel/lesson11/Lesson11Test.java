57232b4652b1b81e8373f5ea414bef1cc5aeaa26
Vadim <kr.vadim29@gmail.com>
2022-12-23 17:56:07 +0300

    Lesson12 A B C

--- src/by/it/group151002/kragel/lesson11/Lesson11Test.java
+++ src/by/it/group151002/kragel/lesson11/Lesson11Test.java
@@ -5,6 +5,10 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class Lesson11Test {
+    private void assertEqualsAndPrint(String message, Object expected, Object actual){
+        System.out.println(actual);
+        assertEquals(message, expected, actual);
+    }
     @Test
     public void A() throws Exception {
         GraphA gA = new GraphA();
@@ -18,11 +22,11 @@ public class Lesson11Test {
         gA.addEdge("D", "G");
         gA.addEdge("D", "H");
         gA.addEdge("G","H");
-        assertEquals("dfs failed", "[A, B, C, F, E, I, D, G, H]", gA.dfsToString());
-        assertEquals("Pre failed", "[0, 1, 2, 12, 4, 3, 13, 14, 6]", gA.getPres().values().toString());
-        assertEquals("Post failed", "[11, 10, 9, 17, 5, 8, 16, 15, 7]", gA.getPosts().values().toString());
-        assertEquals("Trees failed", "[A-B, B-C, C-F, F-E, F-I, D-G, G-H]", gA.getTrees().toString());
-        assertEquals("Backs failed", "[E-A, E-B, H-D]", gA.getBacks().toString());
+        assertEqualsAndPrint("dfs failed", "[A, B, C, F, E, I, D, G, H]", gA.dfsToString());
+        assertEqualsAndPrint("Pre failed", "[0, 1, 2, 12, 4, 3, 13, 14, 6]", gA.getPres().values().toString());
+        assertEqualsAndPrint("Post failed", "[11, 10, 9, 17, 5, 8, 16, 15, 7]", gA.getPosts().values().toString());
+        assertEqualsAndPrint("Trees failed", "[A-B, B-C, C-F, F-E, F-I, D-G, G-H]", gA.getTrees().toString());
+        assertEqualsAndPrint("Backs failed", "[E-A, E-B, H-D]", gA.getBacks().toString());
     }
     @Test
     public void B() throws Exception {
@@ -35,7 +39,7 @@ public class Lesson11Test {
         gB.addOrientEdge("E", "F");
         gB.addOrientEdge("F", "G");
         gB.addOrientEdge("F", "H");
-        assertEquals("Topological failed", "[B, A, C, E, D, F, H, G]", gB.getTopological().toString());
+        assertEqualsAndPrint("Topological failed", "[B, A, C, E, D, F, H, G]", gB.getTopological().toString());
     }
     @Test
     public void C() throws Exception {
@@ -48,11 +52,11 @@ public class Lesson11Test {
         gC.addOrientEdge("E", "F");
         gC.addOrientEdge("F", "G");
         gC.addOrientEdge("F", "H");
-        assertEquals("Pre failed", "[0, 14, 1, 2, 10, 3, 4, 6]", gC.getPres().values().toString());
-        assertEquals("Post failed", "[13, 15, 12, 9, 11, 8, 5, 7]", gC.getPosts().values().toString());
-        assertEquals("All starts failed", "[A, B]", gC.findStarts().toString());
-        assertEquals("All ends failed", "[G, H]", gC.findEnds().toString());
-        assertEquals("Topological failed", "[B, A, C, E, D, F, H, G]", gC.getTopological().toString());
-        assertEquals("Count linear failed", 8, gC.getLinear());
+        assertEqualsAndPrint("Pre failed", "[0, 14, 1, 2, 10, 3, 4, 6]", gC.getPres().values().toString());
+        assertEqualsAndPrint("Post failed", "[13, 15, 12, 9, 11, 8, 5, 7]", gC.getPosts().values().toString());
+        assertEqualsAndPrint("All starts failed", "[A, B]", gC.findStarts().toString());
+        assertEqualsAndPrint("All ends failed", "[G, H]", gC.findEnds().toString());
+        assertEqualsAndPrint("Topological failed", "[B, A, C, E, D, F, H, G]", gC.getTopological().toString());
+        assertEqualsAndPrint("Count linear failed", 8, gC.getLinear());
     }
 }
