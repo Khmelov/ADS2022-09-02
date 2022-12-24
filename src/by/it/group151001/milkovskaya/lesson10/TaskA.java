079800db6f8a6dd39c7c7e035198bb8c550a47f2
Blooife <beusas.23@gmail.com>
2022-12-24 18:40:47 +0300

    done 10

--- src/by/it/group151001/milkovskaya/lesson10/TaskA.java
+++ src/by/it/group151001/milkovskaya/lesson10/TaskA.java
@@ -2,7 +2,7 @@ package by.it.group151001.milkovskaya.lesson10;
 
 import java.util.*;
 
-public class TaskA<E extends Comparable<E>>  implements NavigableSet<E> {
+public class TaskA<E>  implements NavigableSet<E> {
 
     //Создайте аналог дерева TreeSet БЕЗ использования других классов СТАНДАРТНОЙ БИБЛИОТЕКИ
     //Не нужно на массивах это делать или маскируя в поля TreeSet, TreeMap и т.д.
@@ -10,22 +10,158 @@ public class TaskA<E extends Comparable<E>>  implements NavigableSet<E> {
     //в нем также может быть поле элемента E. Далее на этой основе ожидается бинарное дерево.
 
     //Обязательные к реализации методы и конструкторы
-    public TaskA() {
+    public Node<E> root;
+    public int size = 0;
+    public int compareTo(E o1, E o2)
+    {
+        return Integer.compare((int)o1, (int)o2);
     }
+    public class Node<E>{
+        public E data;
+        public Node<E> left, right;
+        public Node(E data){
+            this.data = data;
+        }
+    }
+   // public TaskA() {
+   // }
 
     @Override
     public boolean add(E e) {
+        Node<E> temp = root;
+        Node<E> elem = new Node<>(e);
+        if (!contains(e))
+        {
+            if (root == null)
+            {
+                root = new Node(e);
+//                root = elem;
+                size++;
+                return true;
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    if (compareTo(temp.data, e) > 0)
+                    {
+                        if (temp.left == null)
+                        {
+                            temp.left = elem;
+                            size++;
+                            return true;
+                        }
+                        temp = temp.left;
+                    }
+                    else
+                    {
+                        if (temp.right == null)
+                        {
+                            temp.right = elem;
+                            size++;
+                            return true;
+                        }
+                        temp = temp.right;
+                    }
+                }
+            }
+        }
+
         return false;
     }
 
+    public Node<E> getMinimumKey(Node<E> curr)
+    {
+        while (curr.left != null) {
+            curr = curr.left;
+        }
+        return curr;
+    }
     @Override
     public boolean remove(Object o) {
-        return false;
-    }
+        Node<E> prnt = null;
+        Node<E> curr = root;
+        if(!contains(o)){
+            return false;
+        } else
+        while (curr != null && compareTo(curr.data, (E) o) != 0)
+        {
+            prnt = curr;
+            if (compareTo(curr.data, (E) o) > 0) {
+                curr = curr.left;
+            }
+            else {
+                curr = curr.right;
+            }
+        }
 
+        if (curr.left == null && curr.right == null)
+        {
+            if (curr != root)
+            {
+                if (prnt.left == curr) {
+                    prnt.left = null;
+                }
+                else {
+                    prnt.right = null;
+                }
+            }
+            else {
+                root = null;
+            }
+        }
+
+        else if (curr.left != null && curr.right != null)
+        {
+            Node<E> successor = getMinimumKey(curr.right);
+            E val = successor.data;
+            remove(successor.data);
+            curr.data = val;
+        }
+        else {
+            Node<E> child = (curr.left != null)? curr.left: curr.right;
+            if (curr != root)
+            {
+                if (curr == prnt.left) {
+                    prnt.left = child;
+                }
+                else {
+                    prnt.right = child;
+                }
+            }
+            else {
+                root = child;
+            }
+        }
+        return true;
+    }
+    void Sym(Node<E> root, StringBuilder res)
+    {
+        if (root != null)
+        {
+            Sym(root.left, res);
+            res.append(root.data);
+            res.append(", ");
+            Sym(root.right, res);
+        }
+
+    }
     @Override
     public String toString() {
-        return null;
+        StringBuilder res = new StringBuilder();
+        res.append('[');
+        Sym(root, res);
+        if (res.length() < 2)
+        {
+            res.append(']');
+        }
+        else
+        {
+            res.delete(res.length() - 2, res.length());
+            res.append("]");
+        }
+
+        return res.toString();
     }
     /////////////////////////////////////////////////////////////////////////
     /////////////////////////////////////////////////////////////////////////
@@ -36,6 +172,22 @@ public class TaskA<E extends Comparable<E>>  implements NavigableSet<E> {
 
     @Override
     public boolean contains(Object o) {
+        Node<E> temp = root;
+        while (temp != null)
+        {
+            if (compareTo(temp.data, (E) o) > 0)
+            {
+                temp = temp.left;
+            }
+            else if (compareTo(temp.data, (E) o) < 0)
+            {
+                temp = temp.right;
+            }
+            else if (compareTo(temp.data, (E) o) == 0)
+            {
+                return true;
+            }
+        }
         return false;
     }
 
--- src/by/it/group151001/milkovskaya/lesson10/TaskC.java
+++ src/by/it/group151001/milkovskaya/lesson10/TaskC.java
@@ -2,7 +2,7 @@ package by.it.group151001.milkovskaya.lesson10;
 
 import java.util.*;
 
-public class TaskC<E extends Comparable<E>>  implements NavigableSet<E> {
+public class TaskC<E>  implements NavigableSet<E> {
 
     //Создайте аналог дерева TreeSet БЕЗ использования других классов СТАНДАРТНОЙ БИБЛИОТЕКИ
     //Не нужно на массивах это делать или маскируя в поля TreeSet, TreeMap и т.д.
@@ -10,21 +10,140 @@ public class TaskC<E extends Comparable<E>>  implements NavigableSet<E> {
     //в нем также может быть поле элемента E. Далее на этой основе ожидается бинарное дерево.
 
     //Обязательные к реализации методы и конструкторы
-    public TaskC() {
+    public Node<E> root;
+    public int size = 0;
+    public int compareTo(E o1, E o2)
+    {
+        return Integer.compare((int)o1, (int)o2);
     }
+    public class Node<E>{
+        public E data;
+        public Node<E> left, right;
+        public Node(E data){
+            this.data = data;
+        }
+    }
+    // public TaskB() {
+    // }
 
     @Override
     public boolean add(E e) {
+        Node<E> temp = root;
+        Node<E> elem = new Node<>(e);
+        if (!contains(e))
+        {
+            if (root == null)
+            {
+                root = new Node(e);
+//                root = elem;
+                size++;
+                return true;
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    if (compareTo(temp.data, e) > 0)
+                    {
+                        if (temp.left == null)
+                        {
+                            temp.left = elem;
+                            size++;
+                            return true;
+                        }
+                        temp = temp.left;
+                    }
+                    else
+                    {
+                        if (temp.right == null)
+                        {
+                            temp.right = elem;
+                            size++;
+                            return true;
+                        }
+                        temp = temp.right;
+                    }
+                }
+            }
+        }
         return false;
     }
 
+    public Node<E> getMinimumKey(Node<E> curr)
+    {
+        while (curr.left != null) {
+            curr = curr.left;
+        }
+        return curr;
+    }
     @Override
     public boolean remove(Object o) {
-        return false;
+        Node<E> prnt = null;
+        Node<E> curr = root;
+        if (!contains(o)) {
+            return false;
+        } else{
+            while (curr != null && compareTo(curr.data, (E) o) != 0) {
+                prnt = curr;
+                if (compareTo(curr.data, (E) o) > 0) {
+                    curr = curr.left;
+                } else {
+                    curr = curr.right;
+                }
+            }
+
+            if (curr.left == null && curr.right == null) {
+                if (curr != root) {
+                    if (prnt.left == curr) {
+                        prnt.left = null;
+                    } else {
+                        prnt.right = null;
+                    }
+                } else {
+                    root = null;
+                }
+                size--;
+            } else if (curr.left != null && curr.right != null) {
+               Node<E> successor = getMinimumKey(curr.right);
+                E val = successor.data;
+                remove(successor.data);
+                curr.data = val;
+            } else {
+               Node<E> child = (curr.left != null) ? curr.left : curr.right;
+                if (curr != root) {
+                    if (curr == prnt.left) {
+                        prnt.left = child;
+                    } else {
+                        prnt.right = child;
+                    }
+                } else {
+                    root = child;
+                }
+                size--;
+            }
+
+            return true;
+        }
     }
 
     @Override
     public boolean contains(Object o) {
+        Node<E> temp = root;
+        while (temp != null)
+        {
+            if (compareTo(temp.data, (E) o) > 0)
+            {
+                temp = temp.left;
+            }
+            else if (compareTo(temp.data, (E) o) < 0)
+            {
+                temp = temp.right;
+            }
+            else if (compareTo(temp.data, (E) o) == 0)
+            {
+                return true;
+            }
+        }
         return false;
     }
 
@@ -35,27 +154,83 @@ public class TaskC<E extends Comparable<E>>  implements NavigableSet<E> {
 
     @Override
     public void clear() {
-
+        if(size > 0){
+            this.root = null;
+            size = 0;
+        }
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        if(root != null){
+            return false;
+        }
+        return true;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public E first() {
-        return null;
+        if (size > 0)
+        {
+            Node<E> curr = getMinimumKey(root);
+            return curr.data;
+        }
+        else
+        {
+            return null;
+        }
     }
 
     @Override
     public E last() {
-        return null;
+        if (size > 0)
+        {
+            Node<E> curr = root;
+            while (curr.right != null)
+            {
+                curr = curr.right;
+            }
+            return curr.data;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+
+    void Sym(Node<E> root, StringBuilder res)
+    {
+        if (root != null)
+        {
+            Sym(root.left, res);
+            res.append(root.data);
+            res.append(", ");
+            Sym(root.right, res);
+        }
+
+    }
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append('[');
+        Sym(root, res);
+        if (res.length() < 2)
+        {
+            res.append(']');
+        }
+        else
+        {
+            res.delete(res.length() - 2, res.length());
+            res.append("]");
+        }
+
+        return res.toString();
     }
 
     @Override
@@ -80,19 +255,24 @@ public class TaskC<E extends Comparable<E>>  implements NavigableSet<E> {
 
     @Override
     public E pollFirst() {
+        E found = this.first();
+        if(found != null){
+            remove(found);
+            return found;
+        }
         return null;
     }
 
     @Override
     public E pollLast() {
+        E found = this.last();
+        if(found != null){
+            remove(found);
+            return found;
+        }
         return null;
     }
 
-
-    @Override
-    public String toString() {
-        return null;
-    }
     /////////////////////////////////////////////////////////////////////////
     /////////////////////////////////////////////////////////////////////////
     ////////         Эти методы реализовывать необязательно      ////////////
