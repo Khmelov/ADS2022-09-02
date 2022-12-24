888d091d97a00dec4ac6e1e0d0ff262c9700bcb6
Stas Bybikov <bybikovstanislav@gmail.com>
2022-12-24 04:28:33 +0300

    Lesson 10-12

--- src/by/it/group151002/bybikov/lesson10/TaskA.java
+++ src/by/it/group151002/bybikov/lesson10/TaskA.java
@@ -3,31 +3,174 @@ package by.it.group151002.bybikov.lesson10;
 import java.util.*;
 
 public class TaskA<E extends Comparable<E>>  implements NavigableSet<E> {
-    private Binary_Tree<E> set = null;
 
     //Создайте аналог дерева TreeSet БЕЗ использования других классов СТАНДАРТНОЙ БИБЛИОТЕКИ
     //Не нужно на массивах это делать или маскируя в поля TreeSet, TreeMap и т.д.
     //Можно реализовать класс Node с двумя полями такого же типа (потомки дерева),
     //в нем также может быть поле элемента E. Далее на этой основе ожидается бинарное дерево.
-
+    private E value;
+    private TaskA<E> left;
+    private TaskA<E> right;
     //Обязательные к реализации методы и конструкторы
     public TaskA() {
-        this.set = new Binary_Tree<>();
+        this.value=null;
+        this.left = null;
+        this.right = null;
+    }
+
+    private void SetKey(TaskA<E> e){
+        this.value = e.value;
+        this.left = e.left;
+        this.right =e.right;
+    }
+    public TaskA<E> getMinimumKey(TaskA<E> curr)
+    {
+        while (curr.left.value != null) {
+            curr = curr.left;
+        }
+        return curr;
+    }
+    public TaskA<E> deleteNode(TaskA<E> root, E key)
+    {
+
+      TaskA<E> parent = null;
+
+
+        TaskA<E> curr = root;
+        System.out.println(curr.value);
+
+        while (curr.value != null && !curr.value.equals(key))
+        {
+
+            parent = curr;
+
+
+            if (curr.value.hashCode() > key.hashCode()) {
+                curr = curr.left;
+            }
+            else {
+                curr = curr.right;
+            }
+        }
+
+
+        if (curr.value == null) {
+            return root;
+        }
+
+
+        if (curr.left.value == null && curr.right.value == null)
+        {
+
+            if (curr.value != root.value)
+            {
+                if (parent.left.value == curr.value) {
+                    parent.left.value = null;
+                }
+                else {
+                    parent.right.value = null;
+                }
+            }
+
+            else {
+                root.value = null;
+            }
+        }
+
+
+        else if (curr.left.value != null && curr.right.value != null)
+        {
+
+           TaskA<E> successor = getMinimumKey(curr.right);
+
+
+            E val = successor.value;
+
+
+            deleteNode(root, successor.value);
+
+
+            curr.value = val;
+        }
+
+
+        else {
+
+            TaskA<E> child = (curr.left.value != null)? curr.left: curr.right;
+
+            if (curr.value != root.value)
+            {
+                if (curr.value == parent.left.value) {
+                    parent.left = child;
+                }
+                else {
+                    parent.right = child;
+                }
+            }
+
+
+            else {
+                root = child;
+
+            }
+        }
+
+        return root;
     }
 
     @Override
     public boolean add(E e) {
-        return this.set.insert_Value(e);
+        if(this.value == null)
+        {
+            this.value = e;
+            this.left = new TaskA<>();
+            this.right =  new TaskA<>();
+        }else {
+            if(this.value.hashCode() < e.hashCode()){
+                return this.right.add(e);
+            }
+            if(this.value.hashCode()> e.hashCode())
+            {
+                return this.left.add(e);
+            }
+
+        }
+        return false;
     }
 
     @Override
     public boolean remove(Object o) {
-        return this.set.remove_Value(o);
+        TaskA<E> root =deleteNode(this,(E)o);
+        this.SetKey(root);
+        return root.value == this.value;
     }
+    private void substr(StringBuilder sb)
+    {
+        if(this.value ==null)
+        {
+            return;
+        }
+        this.left.substr(sb);
+        sb.append(this.value);
+        sb.append(", ");
+        this.right.substr(sb);
 
+    }
     @Override
     public String toString() {
-        return this.set.toString();
+        if (this.value == null)
+        {
+            return "[]";
+        }
+        StringBuilder sb= new StringBuilder();
+        sb.append("[");
+        this.left.substr(sb);
+        sb.append(this.value);
+        sb.append(", ");
+        this.right.substr(sb);
+        sb.delete(sb.length()-2,sb.length());
+        sb.append("]");
+        return sb.toString();
     }
     /////////////////////////////////////////////////////////////////////////
     /////////////////////////////////////////////////////////////////////////
@@ -38,7 +181,7 @@ public class TaskA<E extends Comparable<E>>  implements NavigableSet<E> {
 
     @Override
     public boolean contains(Object o) {
-        return set.contains(o);
+        return false;
     }
 
     @Override
@@ -53,12 +196,12 @@ public class TaskA<E extends Comparable<E>>  implements NavigableSet<E> {
 
     @Override
     public boolean isEmpty() {
-        return set.is_Empty();
+        return false;
     }
 
     @Override
     public int size() {
-        return set.size();
+        return 0;
     }
 
     @Override
--- src/by/it/group151002/bybikov/lesson10/TaskB.java
+++ src/by/it/group151002/bybikov/lesson10/TaskB.java
@@ -10,59 +10,279 @@ public class TaskB<E extends Comparable<E>>  implements NavigableSet<E> {
     //в нем также может быть поле элемента E. Далее на этой основе ожидается бинарное дерево.
 
     //Обязательные к реализации методы и конструкторы
+    private E value;
+    private TaskB<E> left;
+    private TaskB<E> right;
+    //Обязательные к реализации методы и конструкторы
     public TaskB() {
+        this.value=null;
+        this.left = null;
+        this.right = null;
+    }
+
+    private void SetKey(TaskB<E> e){
+        this.value = e.value;
+        this.left = e.left;
+        this.right =e.right;
+    }
+    public TaskB<E> getMinimumKey(TaskB<E> curr)
+    {
+        while (curr.left.value != null) {
+            curr = curr.left;
+        }
+        return curr;
+    }
+    public TaskB<E> deleteNode(TaskB<E> root, E key)
+    {
+
+        TaskB<E> parent = null;
+
+
+        TaskB<E> curr = root;
+        System.out.println(curr.value);
+
+        while (curr.value != null && !curr.value.equals(key))
+        {
+
+            parent = curr;
+
+
+            if (curr.value.hashCode() > key.hashCode()) {
+                curr = curr.left;
+            }
+            else {
+                curr = curr.right;
+            }
+        }
+
+
+        if (curr.value == null) {
+            return root;
+        }
+
+
+        if (curr.left.value == null && curr.right.value == null)
+        {
+
+            if (curr.value != root.value)
+            {
+                if (parent.left.value == curr.value) {
+                    parent.left.value = null;
+                }
+                else {
+                    parent.right.value = null;
+                }
+            }
+
+            else {
+                root.value = null;
+            }
+        }
+
+
+        else if (curr.left.value != null && curr.right.value != null)
+        {
+
+            TaskB<E> successor = getMinimumKey(curr.right);
+
+
+            E val = successor.value;
+
+
+            deleteNode(root, successor.value);
+
+
+            curr.value = val;
+        }
+
+
+        else {
+
+            TaskB<E> child = (curr.left.value != null)? curr.left: curr.right;
+
+
+            if (curr.value != root.value)
+            {
+                if (curr.value == parent.left.value) {
+                    parent.left = child;
+                }
+                else {
+                    parent.right = child;
+                }
+            }
+
+
+            else {
+                root = child;
+
+            }
+        }
+
+        return root;
     }
 
     @Override
     public boolean add(E e) {
+        if(this.value == null)
+        {
+            this.value = e;
+            this.left = new TaskB<>();
+            this.right =  new TaskB<>();
+        }else {
+            if(this.value.hashCode() < e.hashCode()){
+                return this.right.add(e);
+            }
+            if(this.value.hashCode()> e.hashCode())
+            {
+                return this.left.add(e);
+            }
+
+        }
         return false;
     }
 
     @Override
     public boolean remove(Object o) {
-        return false;
+        TaskB<E> root =deleteNode(this,(E)o);
+        this.SetKey(root);
+        return root.value == this.value;
+    }
+    private void substr(StringBuilder sb)
+    {
+        if(this.value ==null)
+        {
+            return;
+        }
+        this.left.substr(sb);
+        sb.append(this.value);
+        sb.append(", ");
+        this.right.substr(sb);
+
+    }
+    @Override
+    public String toString() {
+        if (this.value == null)
+        {
+            return "[]";
+        }
+        StringBuilder sb= new StringBuilder();
+        sb.append("[");
+        this.left.substr(sb);
+        sb.append(this.value);
+        sb.append(", ");
+        this.right.substr(sb);
+        sb.delete(sb.length()-2,sb.length());
+        sb.append("]");
+        return sb.toString();
     }
 
     @Override
     public boolean contains(Object o) {
+        if(this.isEmpty()){return false;}
+        TaskB<E> curr = this;
+        while (curr.value != null)
+        {
+            if(curr.value.equals(o))
+            {
+                return true;
+            }
+                if(curr.value.hashCode()>o.hashCode())
+                {
+                    curr = curr.left;
+                }
+               else {
+                    curr = curr.right;
+                }
+        }
         return false;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        Stack<E> stack = new Stack<>();
+        final TaskB<E>[] curr = new TaskB[]{this};
+        Iterator<E> iterator = new Iterator<E>() {
+            @Override
+            public boolean hasNext() {
+
+                return (!stack.isEmpty() || curr[0].value != null);
+            }
+
+            @Override
+            public E next() {
+                while (curr[0].value != null) {
+                    stack.push(curr[0].value);
+                    curr[0] = curr[0].left;
+                }
+
+                curr[0] = (TaskB<E>) stack.pop();
+                TaskB<E> node = curr[0];
+                curr[0] = curr[0].right;
+
+                return node.value;
+            }
+        };
+        return iterator;
     }
 
     @Override
     public void clear() {
-
+        if(this.value != null)
+        {
+            this.left.clear();
+            this.right.clear();
+            this.value = null;
+            this.left = null;
+            this.right = null;
+        }
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.value == null;
     }
 
     @Override
     public int size() {
-        return 0;
+        int S = 0;
+        if(this.isEmpty())
+        {
+            return S;
+        }
+        TaskB<E> left = this.left;
+        TaskB<E> right  =this.right;
+        S++;
+        if(left.value !=null)
+        {
+           S+=left.size();
+        }
+        if(right.value != null)
+        {
+            S+=right.size();
+        }
+        return S;
     }
 
     @Override
     public E first() {
-        return null;
+       TaskB<E> curr = this;
+       while (curr.left.value != null)
+       {
+           curr = curr.left;
+       }
+       return curr.value;
     }
 
     @Override
     public E last() {
-        return null;
+        TaskB<E> curr = this;
+        while(curr.right.value !=null)
+        {
+            curr = curr.right;
+        }
+        return curr.value;
     }
 
-
-    @Override
-    public String toString() {
-        return null;
-    }
     /////////////////////////////////////////////////////////////////////////
     /////////////////////////////////////////////////////////////////////////
     ////////         Эти методы реализовывать необязательно      ////////////
